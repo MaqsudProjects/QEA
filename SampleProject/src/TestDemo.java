7493224e31395b2a8b2a65e71698578e7c1e352c
Maqsud Kukaswadia <Maqsud Kukaswadia@DESKTOP-AOEP0U2>
2023-12-26 17:22:29 +0530

    Display Message

--- SampleProject/src/TestDemo.java
+++ SampleProject/src/TestDemo.java
@@ -1,10 +1,10 @@
 import org.testng.annotations.Test;
 
 public class TestDemo {
-	@Test
-	public void testCase1()
-	{
-		System.out.println("TestCase1 is Successfully passed!");
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Demo");
 	}
 
 }
